eb77954f86992cb57e82c64294e261ff2e463524
Justin Verthein <Justinmackay88@hotmail.com>
2023-05-02 11:26:43 -0700

    test

--- src/Home.js
+++ src/Home.js
@@ -4,9 +4,9 @@ import SurfSpot from './SurfSpot'
 
 const Home = () => {
 const [spots, setSpots] = useState([
-    {title:"Honolua bay", body:"World famous surf spot", location:"Maui,Hawaii", id: 1},
-    {title:"Breakwall", body:"Famous beginner surf spot", location:"Maui,Hawaii", id: 2},
-    {title:"Jaws", body:"Biggest waves in the world", location:"Maui,Hawaii", id: 3}
+    { title: 'Honolua bay', body: 'World famous surf spot', location: 'Maui,Hawaii', id: 1},
+    { title: 'Breakwall', body: 'Famous beginner surf spot', location: 'Maui,Hawaii', id: 2},
+    { title: 'Jaws', body: 'Biggest waves in the world', location: 'Maui,Hawaii', id: 3}
 ]);
 
     return(
--- src/SurfSpot.js
+++ src/SurfSpot.js
@@ -1,15 +1,19 @@
 import React from 'react'
 
 const SurfSpot = (props) => {
-const spots = props.spots
+    //console.log(props)
+const spots = props
+console.log(spots)
+
+const spottest = spots.map((spot) => (console.log(spot))
+    // <div className="spot" key={spot.id}>
+    //     <h2>{ spot.title }</h2>
+    //     <p> Location { spot.location }</p>
+    // </div>
+)
     return (
         <div className="surf-spot"> 
-            {spots.map((spot) => (
-                <div className="spot-preview" key={spot.id}>
-                    <h2>{ spot.title }</h2>
-                    <p> Location { spot.location }</p>
-                </div>
-            ))}
+            {spottest}
         </div>
     );
 }
