39384d4bff5a5a4d6149cc61282ab1317c4ec977
Justin Verthein <Justinmackay88@hotmail.com>
2023-05-02 23:32:46 -0700

    added ability to delete surf spots and made another redirect after deleting

--- src/SurfDetails.js
+++ src/SurfDetails.js
@@ -1,9 +1,18 @@
-import { useParams } from "react-router-dom/cjs/react-router-dom";
+import { useParams, useHistory} from "react-router-dom/cjs/react-router-dom";
 import useFetch from "./useFetch";
 
 const SurfDetails = () => {
     const { id } = useParams();
     const { data: spot, error, isLoading} = useFetch('http://localhost:3000/spots/' + id)
+    const redirect = useHistory();
+
+    const handleClick= () => {
+        fetch('http://localhost:3000/spots/' + spot.id, {
+            method: 'DELETE'
+        }).then(() => {
+            redirect.push('/');
+        })
+    }
     return ( 
         <div className="surf-details">
            {isLoading && <div> Loading...</div>}
@@ -13,6 +22,7 @@ const SurfDetails = () => {
                 <h2> {spot.title}</h2>
                 <p> Location {spot.location}</p>
                 <div>{spot.body}</div>
+                <button onClick={handleClick}> delete </button>
             </article>
            )}
         </div>
