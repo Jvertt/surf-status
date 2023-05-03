eff52cf63e498ffdff0f608162ba20e2a8a8c20d
Justin Verthein <Justinmackay88@hotmail.com>
2023-05-02 21:39:40 -0700

    working on adding ability to add a new surf spot

--- src/NewSpot.js
+++ src/NewSpot.js
@@ -4,7 +4,22 @@ const NewSpot = () => {
     return(
         <div className="new-spot"> 
         <h2> Add a new spot!</h2>
-       
+        <form> 
+            <label> Surf Spot </label>
+            <input
+            type="text"
+            required
+            />
+            <label> Surf body</label>
+            <textarea
+            required></textarea>
+            <label> Location </label>
+            <select>
+                <option value= 'maui'> maui </option>
+                <option value= 'hawaii'> maui </option>
+            </select>
+            <button> add spot</button>
+        </form>
         </div>
     )
 }
